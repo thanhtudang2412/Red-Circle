348e9aa65ae6b3a83f30e40a5e493b6492e27097
thanhtudang2412 <thanhtudang2412@users.noreply.github.com>
2021-02-24 21:58:13 +0700

    1.0.1

--- app/src/main/java/com/example/redcircle/MainActivity.java
+++ app/src/main/java/com/example/redcircle/MainActivity.java
@@ -2,14 +2,32 @@ package com.example.redcircle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mLoginBtn, mRegisterBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLoginBtn = findViewById(R.id.login_btn);
+        mRegisterBtn = findViewById(R.id.register_btn);
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return super.onSupportNavigateUp();
     }
 }
---
+++ app/src/main/java/com/example/redcircle/RegisterActivity.java
@@ -0,0 +1,20 @@
+package com.example.redcircle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Create Account");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+
+    }
+}
